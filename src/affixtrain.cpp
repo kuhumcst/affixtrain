ec91f27b711d0dd5682c4a9550e41092a05166a9
Bart Jongejan <bart@cst.dk>
2014-07-04 16:25:57 +0200

    Version number increased

--- src/affixtrain.cpp
+++ src/affixtrain.cpp
@@ -20,7 +20,7 @@ along with AFFIXTRAIN; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#define VERSION "1.6"
+#define VERSION "1.7"
 
 static int PERC = 1;
 static int RECURSE = 1;

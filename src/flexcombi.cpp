43735d269834a254fe64b7b860a059ee541fc6c6
Bart Jongejan <bart@cst.dk>
2015-02-15 19:02:56 +0100

    printf stmnt in flexcombi: filenames

--- src/flexcombi.cpp
+++ src/flexcombi.cpp
@@ -1130,6 +1130,7 @@ bool flexcombi(const char * bestflexrules, const char * nextbestflexrules, const
     {
     fileBuffer FileBuffer;
     fileBuffer NextFileBuffer;
+    printf("flexcombi (V3): %s + %s -> %s\n", bestflexrules, nextbestflexrules, combinedflexrules);
     if (!FileBuffer.readRules(bestflexrules))
         {
         printf("Error (flexcombi): Cannot open %s for reading\n",bestflexrules);

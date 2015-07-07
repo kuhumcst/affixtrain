57521b6aac3b8b1ec45483ed5f1b68807a23d5cb
Bart Jongejan <bart@cst.dk>
2015-07-07 20:24:04 +0200

    updated version no.

--- src/affixtrain.cpp
+++ src/affixtrain.cpp
@@ -20,7 +20,7 @@ along with AFFIXTRAIN; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#define VERSION "2.1"
+#define VERSION "2.2"
 
 #include "affixtrain.h"
 #include "testrules.h"

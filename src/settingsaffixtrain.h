75f6676f6c7ad0bec5f2fc3c533ad5519b2e10c0
Bart Jongejan <bart@cst.dk>
2015-07-16 13:59:22 +0200

    Set AMBIGUOUS back to 1

--- src/settingsaffixtrain.h
+++ src/settingsaffixtrain.h
@@ -43,7 +43,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 // Set this number as high as possible. A high value entails very heavy use 
 // of memory!
 
-#define AMBIGUOUS 0
+#define AMBIGUOUS 1
 // AMBIGUOUS 1
 // Handle ambiguous data by redoing training a second time without the
 // homographs that were handled in the first round.
